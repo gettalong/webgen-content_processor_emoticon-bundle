ab2dfd2404b9a3d52d89dcfaacc45e32ef803e4c
Thomas Leitner <t_leitner@gmx.at>
2013-08-25 11:27:26 +0200

    Fixed bundle loading functionality

--- lib/webgen/bundle/content_processor_emoticon/init.rb
+++ lib/webgen/bundle/content_processor_emoticon/init.rb
@@ -2,7 +2,6 @@ require_relative('emoticon')
 
 website.ext.content_processor.register('Emoticon')
 
-option('content_processor.emoticon.theme', nil,
-       "The name of an image emoticon theme, unicode (for text emoticons) or nil")
+option('content_processor.emoticon.theme', nil)
 
 mount_passive('emoticons/', '/images/emoticons/', '**/')
